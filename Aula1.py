230718663394a9a62645f94cdc02661aa622f8e4
Renata <renata.rodriguesdasilva@outlook.com>
2024-08-27 23:43:47 -0300

    aula1

---
+++ Aula1.py
@@ -0,0 +1,13 @@
+# Operadores Aritméticos
+produto1 = 20
+produto2 = 10
+print(produto1+produto2)
+print(produto1-produto2)
+print(produto1/produto2)
+print(produto1//produto2)
+print(produto1*produto2)
+print(produto1**produto2)
+x = 10 + 5 * 4
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(x)
+print(y)
