16c353febd529307a8598a1ccd4ed89c4de44f4c
Renata <renata.rodriguesdasilva@outlook.com>
2024-09-18 20:34:59 -0300

    =>

---
+++ Aula1-Operadores-logicos.py
@@ -0,0 +1,8 @@
+saldo_conta = 400
+jogo = 350
+nome_jogo = "Mario"
+      #true ou false           #true ou false
+if saldo_conta >= jogo and nome_jogo == "Mario" or nome_jogo == "Sonic":
+    print("vou comprar")
+else:
+    print("não vou comprar")
