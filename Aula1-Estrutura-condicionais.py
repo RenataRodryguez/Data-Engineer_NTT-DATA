44d37841368f5261ef113572f12cb1add0b0f31d
Renata <renata.rodriguesdasilva@outlook.com>
2024-09-25 20:34:19 -0300

    edit

--- Aula1-Estrutura-condicionais.py
+++ Aula1-Estrutura-condicionais.py
@@ -1,14 +1,18 @@
-conta_simples = True 
+conta_normal = True 
 conta_salario = False
-
-saldo = 200
+saldo = 2000
 saque = 2500
 cheque_especial = 450
 
-if conta_simples >= saque:
-    print('saque realizado com sucesso')
-elif saque <= (saldo + cheque_especial):
-    print('saque realizado com uso do cheque especial!')
+if conta_normal:
+    if saldo >= saque:
+        print('Saque realizado com sucesso')
+    elif saque <= (saldo + cheque_especial):
+        print('Saque realizado com uso do cheque especial!')
+    else:
+        print('Não foi possível realizar o saque, saldo insuficiente!')
 elif conta_salario:
     if saldo >= saque:
-        print('saque realizado com sucesso')
+        print('Saque realizado com sucesso')
+    else:
+        print('Saldo insuficiente!')
